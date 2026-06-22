7ed2d0a682ee808ec3f039a617ef4fa880279e75
Felipe <cc25130@g.unicamp.br>
2026-06-22 11:59:49 -0300

    oi

--- apCalculadora/IRegistro.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-using System.IO;
-
-public interface IRegistro<T> where T : IComparable<T>, new()
-{
-    bool LerRegistro(StreamReader arq);
-    void GravarRegistro(StreamWriter arq);
-}
